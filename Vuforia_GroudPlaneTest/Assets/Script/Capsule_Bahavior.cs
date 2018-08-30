7ed0b244f18c67fae038d15eec90388474ae9b2a
octopopo <godpopo34@gmail.com>
2018-08-30 14:43:07 -0700

    A bunch of stuff
    Somehow it works now on my pixel

---
+++ Vuforia_GroudPlaneTest/Assets/Script/Capsule_Bahavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule_Bahavior : MonoBehaviour {
+
+    // Use this for initialization
+    public MeshRenderer m_MR;
+    public Collider m_CL;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("My Mesh Renderer status: " + m_MR.enabled);
+        Debug.Log("My Collider status: " + m_CL.enabled);
+	}
+}
